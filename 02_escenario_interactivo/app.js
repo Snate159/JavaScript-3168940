4fe154e4df780fe74bfcf322dbf5e4f0a6411ecb
Snate159 <santi.rrplay@gmail.com>
2025-10-21 17:19:20 -0500

    02_escenario_interactivo

--- 02_escenario_interactivo/app.js
+++ 02_escenario_interactivo/app.js
@@ -1,2 +1,27 @@
-    const aumentos = document.querySelectorAll("aumentos")
-    const tablero = document.querySelector("contador")
+    const aumentos = document.querySelectorAll(".aumentos")
+    const aumentos2 = document.querySelectorAll(".estrella")
+    const tablero = document.querySelector("#contador-monedas")
+    const tablero2 = document.querySelector("#contador-estrellas")
+    let counter = 0
+
+    aumentos.forEach(item => {
+
+        item.addEventListener("click", () => {
+            item.style.filter = "grayscale(1)"
+            item.classList.add("saltar")
+            counter++
+            tablero.textContent = counter
+        })
+    })
+
+    aumentos.forEach(item => {
+
+        item.addEventListener("click", () => {
+            item.style.filter = "grayscale(1)"
+            item.classList.add("saltar")
+            counter++
+            tablero2.textContent = counter
+        })
+        
+    })
+
