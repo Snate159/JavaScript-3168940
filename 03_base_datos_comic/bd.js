6cce2618915c55a18f1a0860dbca17d204dcee97
Snate159 <santi.rrplay@gmail.com>
2025-11-05 17:24:21 -0500

    03_base_datos_comic

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,40 @@
+let comic = {
+  "nombreComic": "El eco Olvidado",
+  "numeroEpisodios": "Metro City",
+  "genero": "Fantasia, drama",
+  "sinopsis": "Un joven de 17 años decide ir a un museo abandonado donde encontrar un peculiar mentor del pasado",
+  "autores": {"Santiago", "Salome" "Luis"}
+  "portadaComic":
+  "year": 2025,
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": "Ua nota joven",
+      "personajes": ["Raizura", "Abuela", "Reportero", "Constructores"]
+      "portada": ".img7portada.jpg",
+      "descripcion": "Conocemos a raizura, un joven lleno de curiosidad y fascinacion por la musica, decide investigar un museo de musica"
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
