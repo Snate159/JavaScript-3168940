6f7116409f7806bbc5d670ba3d65995f1027a70b
Snate159 <santi.rrplay@gmail.com>
2025-11-06 17:17:12 -0500

    03_base_datos_comic

--- 03_base_datos_comic/bd.js
+++ 03_base_datos_comic/bd.js
@@ -3,38 +3,30 @@ let comic = {
   "numeroEpisodios": "Metro City",
   "genero": "Fantasia, drama",
   "sinopsis": "Un joven de 17 años decide ir a un museo abandonado donde encontrar un peculiar mentor del pasado",
-  "autores": {"Santiago", "Salome" "Luis"}
+  "autores": {"Santiago", "Salome", "Luis"} ,
   "portadaComic":
   "year": 2025,
   "capitulos": [
     {
       "id": 1,
       "nombre": "Ua nota joven",
-      "personajes": ["Raizura", "Abuela", "Reportero", "Constructores"]
-      "portada": ".img7portada.jpg",
-      "descripcion": "Conocemos a raizura, un joven lleno de curiosidad y fascinacion por la musica, decide investigar un museo de musica"
+      "personajes": ["Raizura", "Abuela", "Reportero", "Constructores"],
+      "portada": ".img/portada.jpg",
+      "descripcion": "Conocemos a raizura, un joven lleno de curiosidad y fascinacion por la musica decide investigar un museo de musica"
     },
     {
-      "name": "Madame Uppercut",
-      "age": 39,
-      "secretIdentity": "Jane Wilson",
-      "powers": [
-        "Million tonne punch",
-        "Damage resistance",
-        "Superhuman reflexes"
-      ]
+      "id": 2,
+      "nombre": "El fantasma de la sabrosura",
+      "personajes": ["Raizura", "Fruko"],
+      "portada": ".img/portada.jpg",
+      "Descripcion": "Raizura conoce a fruko, un fantasma que decide ayudarlo a revivir su musica",
     },
     {
-      "name": "Eternal Flame",
-      "age": 1000000,
-      "secretIdentity": "Unknown",
-      "powers": [
-        "Immortality",
-        "Heat Immunity",
-        "Inferno",
-        "Teleportation",
-        "Interdimensional travel"
-      ]
+      "id": 3,
+      "nombre": "El primer y ultimo concierto",
+      "personajes": ["Raizura", "Fruko", "Abuela", "Trabajadores", "Ciudadanos"],
+      "portada": ".img/portada.jpg",
+      "Descripcion": "Raizura decide hacer un concierto para evitar una tragedia",
     }
   ]
 }
